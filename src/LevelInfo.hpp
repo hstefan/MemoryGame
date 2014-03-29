7c22ab5bd7ce78522abe9d4a97a4f6287a43a917
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-03-29 00:22:43 -0300

    Adding exception in the level info parser to load "hidden" sprite.

--- src/LevelInfo.hpp
+++ src/LevelInfo.hpp
@@ -9,6 +9,7 @@ struct LevelInfo {
 	LevelInfo(const char* levelName, const char* descFile);
 	
 	std::vector<std::string> sprites;
+	std::string hiddenSprite;
 	const std::string levelName;
 	const std::string descriptFile;
 	size_t width;
