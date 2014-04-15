eb8f7dbc8c0ca70cfe0b56ec40df80273a79b051
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-04-14 22:32:32 -0300

    Adding premake5 configuration file.

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+solution "MemoryGame"
+	configurations { "release", "debug" }
+
+	flags { "FatalWarnings" }
+	warnings "Extra"
+
+	targetdir "bin"
+
+	configuration "debug"
+		defines { "DEBUG" }
+		flags { "symbols" }
+		optimize "Off"
+	
+	configuration "release"
+		defines { "NDEBUG" }
+		optimize "On"
+	
+	configuration "gmake"
+		buildoptions { "-std=c++11", "-Wall"}
+
+	project "MemoryGame"
+		kind "ConsoleApp"
+		language "c++"
+		files { "src/**.cpp", "src/**.hpp", "src/**.c", "src/**.cpp" }
+		includedirs { "src" }
+		links { "SDL2", "SDL2_image", "SDL2_ttf" }
+
